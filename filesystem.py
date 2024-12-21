fef286ccca82dd009cf20462b067daca1d0fdf29
Venquieu <Richard_wj237@outlook.com>
2024-12-21 23:12:57 +0800

    feat: new APIs(Struct run_parallel read_file write_file)

--- filesystem.py
+++ filesystem.py
@@ -1,5 +1,7 @@
 import os
+import json
 from glob import glob
+from typing import Union, List, Dict
 
 
 def make_dataset(root: str, ext: str = None, followlinks: bool = False):
@@ -41,12 +43,46 @@ def sync_rm(source_dir: str, target_dir: str, strict: bool = False):
             os.remove(target_file)
 
 
-def read_text_file(path: str):
+def read_text_file(path: str) -> list:
+    """
+    Load text file -> strip each line -> return list of lines.
+    """
     with open(path, "r", encoding="utf-8") as f:
         lines = f.readlines()
     return [line.strip() for line in lines]
 
 
-def write_text_file(path: str, lines: list, mode: str = "w"):
+def write_text_file(path: str, lines: list, mode: str = "w") -> None:
     with open(path, mode=mode, encoding="utf-8") as f:
         f.write("\n".join(lines) + "\n")
+
+
+def read_json_file(path: str) -> dict:
+    with open(path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+
+
+def write_json_file(path: str, data: dict, mode: str = "w", indent: int = 4) -> None:
+    with open(path, mode=mode, encoding="utf-8") as f:
+        json.dump(data, f, indent=indent)
+
+
+def read_file(path: str) -> Union[List, Dict]:
+    ext = os.path.splitext(path)[1]
+    if ext == ".txt":
+        return read_text_file(path)
+    elif ext == ".json":
+        return read_json_file(path)
+    else:
+        raise ValueError(f"Unsupported file type: {ext}")
+
+
+def write_file(path: str, data: Union[List, Dict], mode: str = "w", **kargs) -> None:
+    ext = os.path.splitext(path)[1]
+    if ext == ".txt":
+        write_text_file(path, data, mode, **kargs)
+    elif ext == ".json":
+        write_json_file(path, data, mode, **kargs)
+    else:
+        raise ValueError(f"Unsupported file type: {ext}")
--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+from typing import Union, List, Tuple, Any
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from tqdm import tqdm
+
+
 class Struct(object):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
@@ -12,3 +18,24 @@ class Struct(object):
     
     def __repr__(self):
         return str(self.__dict__)
+
+
+def run_parallel(func: Any, args_list: Union[List, Tuple], max_workers:int = 32):
+    """"
+    Run a function in parallel using ThreadPoolExecutor.
+
+    Args:
+        func (Any): The function to be executed in parallel.
+        args_list (Union[List, Tuple]): A list or tuple of arguments to be passed to the function.
+        max_workers (int, optional): The maximum number of workers to use. Defaults to 32.
+
+    Returns:
+        List: A list of results returned by the function for each argument in the args_list.
+    """
+    max_workers = min(max_workers, len(args_list))
+    results = []
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(func, *args) for args in args_list]
+        for future in tqdm(as_completed(futures), total=len(futures)):
+            results.append(future.result())
+    return results
